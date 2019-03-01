6e168a9225ae02b26e316e1f70b0a276da579684
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-03-01 09:06:40 -0300

    Added examples

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## 0.1.0
+
+* Added example files
+
+## 0.0.2
+
+* Changed some files and fixed bugs
+
 ## 0.0.1
 
 * Added expovariate and uniform distributions
---
+++ example/example.dart
@@ -0,0 +1,16 @@
+library random_distribuitions;
+
+import 'package:random_distribuitions/src/random.dart';
+
+void main(){
+
+  var testUniform = random();
+
+  print(testUniform.uniform(1,2));
+
+  var rand = new random();
+
+  print(rand.expovariate(3));
+
+
+}
